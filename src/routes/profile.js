1373045db12c6e1987f915a38af54831004dcaae
Arpita Mandloi <arpitamandloi369@gmail.com>
2026-06-13 13:00:23 +0530

    added new API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,13 +2,37 @@ const express = require("express");
 const profileRouter = express.Router();
 const User = require("../models/user");
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validate");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("something went wrong");
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+
+    await loggedInUser.save();
+
+    res.json({
+       message : `${loggedInUser.firstName} , your profile updated successfully!!`,
+       data : loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
   }
 });
 
